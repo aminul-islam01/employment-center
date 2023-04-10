4bf380140462486ee4250881d6affe3494014d39
aminul-islam01 <abiddut953@gmail.com>
2023-04-10 09:28:04 +0600

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbars from './Components/Navbar/Navbars';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbars></Navbars>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbars.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Navbars = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">Employment center</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0 mx-lg-auto"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                    </Nav>
+                    
+                        <Button variant="outline-success">Search</Button>
+    
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+
+};
+
+export default Navbars;
